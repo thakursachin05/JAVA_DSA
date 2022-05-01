6df8bacd0176ca374d3dbb1be6c4540319171446
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-01 22:04:14 +0530

    First Program in Java
    Java uses pretty simple syntax , start with public class YOUR_FILE_NAME and there after main function from where the program starts executing...

---
+++ Day_1/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	
+	
+	public static void main(String args[]) {
+		//if  you use println java will put a newline at the end.
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+		System.out.println("Hello World");
+	}
+}
