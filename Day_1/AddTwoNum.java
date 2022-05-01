d2a0ab4544992ebd9118ee72a6d4d5c0e97ef934
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-01 22:10:29 +0530

    Taking input and adding two numbers

---
+++ Day_1/AddTwoNum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class AddTwoNum {
+
+	public static void main(String[] args) {
+//		int i = 10;
+//		int j = 15; 
+		// 4 byte
+//		short s = 123; // 2 byte
+//		long l = 12345; // 8 byte
+//		double d = 1.23; // 8 byte
+//		float f; //4 bytes
+//		char c = 'a'; //2 bytes
+//		boolean b = true;
+//		boolean others = false;
+		
+//		int k = i+j;
+		
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		int j = scan.nextInt();
+		
+		int k = i+j;
+		
+		System.out.println(k);
+	}
+
+}
