e57b22abe765812cb6faa544cde7e1ccf0fb0358
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-01 22:07:19 +0530

    DataTypes in Java
    There are some predefined Datatypes in Java.
    Such as: -
    int  - storing integer
    float - it stores decimal number
    double - same as float but for small decimal value
    char -  stores character "a-z" and many more..

---
+++ Day_1/DataType.java
@@ -0,0 +1,31 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			char c ='a';
+//			System.out.println(c+3);
+			
+			int i = c+10;
+//			System.out.println(i);
+			i=c;
+			char abc = (char)i;
+			long l = i;
+			l=2346754786789l;
+			i=(int)l;
+			System.out.println(l);
+			System.out.println(i);
+			
+			
+			double d = i;
+//			l = d
+			
+			float f = 1.23f;
+//			f=d;
+			d=f;
+			int j = 100;
+			
+			
+	}
+
+}
