e3251eda82f89e066288a202db0894fd2420cc5e
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-01 22:11:07 +0530

    taking input from user

---
+++ Day_1/Relational.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Relational {
+
+	public static void main(String []args) {
+		Scanner s  = new Scanner(System.in);
+		int i = s.nextInt();
+		int j = s.nextInt();
+		
+		boolean info = (i==j);
+		System.out.print(info);
+	}
+}
