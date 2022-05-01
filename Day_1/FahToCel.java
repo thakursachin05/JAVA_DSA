16b350ac8c5cfdbbe8d35d85d8ee2ced783c3975
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-01 22:13:54 +0530

    Program to covert temp

---
+++ Day_1/FahToCel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class FahToCel {
+	public static void main(String []args) {
+		Scanner s = new Scanner(System.in);
+		int f = s.nextInt();
+//		int cel = (5/9)*(f-32);
+//		int cel = (5*(f-32))/9;
+		int cel = (int)((5.0/9)*(f-32));
+		System.out.println(cel); 
+		// System.out.println(9%5);
+		// System.out.println('A' + 1);
+		// System.out.println('a'+'b');
+		// char c ='a';
+		// char i = (char)(c+3);
+		// System.out.println(i);
+		
+	}
+}
