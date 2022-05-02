1b7681b3b41e7410415abdbb4508cfe6b80be61f
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-02 22:04:17 +0530

    Arrays in Java

---
+++ Array/ArrayUse.java
@@ -0,0 +1,17 @@
+
+public class ArrayUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int []arr = new int[10];
+//		arr[3]=15;
+//		System.out.println(arr[4]);
+		char []ch = new char[10];
+		double []db = new double[10];
+		System.out.println(ch[4]);
+		System.out.println(db [4]);
+		boolean arr[] = new boolean[5];
+		System.out.println(arr[0]);
+	}	
+
+}
