d5dfe85c2cedcf4405cbe9c0683167838c1de5d2
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-07 22:49:54 +0530

    Binary Search Implementation

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,27 @@
+
+public class BinarySearch {
+	
+    public static int binarySearch(int[] arr, int x) {
+    	//Your code goes here
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(arr[mid]==x){
+                return mid;
+            }
+            else if(arr[mid]>x){
+                end = mid-1;
+            }
+            else start = mid+1;
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+    	int []arr = {1,2,4,5,6,78};
+    	int index = binarySearch(arr,6);
+    	System.out.println(index);
+    }
+}
