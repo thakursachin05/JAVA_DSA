0bdbf07c614ffa5b9778afa2880547c993e91bdb
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-30 23:15:31 +0530

    Create pattern3.java

---
+++ Pattern/pattern3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i=1;
+		while(i<=n) {
+			int j = 1;
+			while(j<=n) {
+				System.out.print(n-j+1);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
