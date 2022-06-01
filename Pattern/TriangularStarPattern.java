4c433a76ccd1b670ea58821b055a263517971ed4
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-01 22:29:07 +0530

    Create TriangularStarPattern.java

---
+++ Pattern/TriangularStarPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TriangularStarPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i=1;
+		int startingNumber = 1;
+		while(i<=n) {
+			int j = 1;
+			
+			while(j<=i) {
+				System.out.print('*');
+				startingNumber++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
