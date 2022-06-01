0739fe89dd91fd663a60e85f22e52b3a3f0658ad
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-01 22:28:59 +0530

    Create ReverseNumberPattern.java

---
+++ Pattern/ReverseNumberPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseNumberPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i=1;
+		
+		while(i<=n) {
+			int j = 1;
+			int startingNumber = i;
+			while(j<=i) {
+				System.out.print(startingNumber);
+				startingNumber--;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
