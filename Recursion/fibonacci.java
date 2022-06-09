93a949fe27d74a30851f73ed99d312a80e71449b
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-09 22:40:52 +0530

    Create fibonacci.java

---
+++ Recursion/fibonacci.java
@@ -0,0 +1,12 @@
+
+public class fibonacci {
+	public static int fib(int n) {
+		if(n==0 || n==1) return n;
+		return fib(n-1)+fib(n-2);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(fib(10));
+	}
+
+}
