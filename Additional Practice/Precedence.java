fb19ee8a847a2e705c25bc82530037bc39720898
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-02 23:25:49 +0530

    Create Precedence.java

---
+++ Additional Practice/Precedence.java
@@ -0,0 +1,15 @@
+
+public class Precedence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		int j=6;
+		int c = 5*2/8;
+//		boolean a = i+5>5 && j>6;
+//		System.out.print(a);
+		System.out.print(c);
+	}
+	
+
+}
