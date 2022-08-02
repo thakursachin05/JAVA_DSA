d496b7e8b6321a2f142d2de4070fe0363539c30a
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-02 23:25:47 +0530

    Create ArthmaticOperators.java

---
+++ Additional Practice/ArthmaticOperators.java
@@ -0,0 +1,12 @@
+
+public class ArthmaticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 3;
+		int c = a/(2*b);
+		System.out.println(c);
+	}
+
+}
